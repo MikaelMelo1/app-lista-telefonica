e54cbb3367b1c588d2fac1194c2b1835dbdcd2c3
Mikael Melo <mikaelsilva1940@hotmail.com>
2022-12-19 21:32:35 -0300

    Grid do App

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { FlatList, Image, StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const data = [
+  {
+    id: "1",
+    name: "Mercado Tschiller",
+    endereco: "Rua Juvencio Cardoso",
+    numero: "(51) 3723-1313",
+    url: "https://ocorreio.com.br/wp-content/uploads/2019/12/Novo-768x330.jpg",
+  },
+
+  {
+    id: "2",
+    name: "Mercado Tschiller",
+    endereco: "Rua Juvencio Cardoso",
+    numero: "(51) 3723-1313",
+    url: "https://ocorreio.com.br/wp-content/uploads/2019/12/Novo-768x330.jpg",
+  },
+  {
+    id: "3",
+    name: "Mercado Tschiller",
+    endereco: "Rua Juvencio Cardoso",
+    numero: "(51) 3723-1313",
+    url: "https://ocorreio.com.br/wp-content/uploads/2019/12/Novo-768x330.jpg",
+  },
+
+  {
+    id: "4",
+    name: "Mercado Tschiller",
+    endereco: "Rua Juvencio Cardoso",
+    numero: "(51) 3723-1313",
+    url: "https://ocorreio.com.br/wp-content/uploads/2019/12/Novo-768x330.jpg",
+  },
+];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    marginTop: 20,
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 10,
+    flexDirection: "column",
+    backgroundColor: "#ADD8E6",
+    margin: 2,
+    borderRadius: 50,
+  },
+  image: {
+    width: 180,
+    height: 180,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  lugar: {
+    backgroundColor: "green",
+    alignItems: "center",
+    textAlign: "center",
   },
 });
+
+const App = () => (
+  <FlatList
+    data={data}
+    renderItem={({ item }) => (
+      <>
+        <View style={styles.itemContainer}>
+          <Image source={{ uri: item.url }} style={styles.image} />
+          <Text style={styles.text}>{item.name}</Text>
+          <Text style={styles.text}>{item.endereco}</Text>
+          <Text style={styles.text}>{item.numero}</Text>
+        </View>
+      </>
+    )}
+    keyExtractor={(item) => item.id}
+  />
+);
+
+export default App;
---
+++ src/lista/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+
+export default class Lista extends Component {
+  render() {
+    return (
+      <View style={styles.containerLista}>
+        <Text style={styles.titleLista}>{this.props.data.nome}</Text>
+        <Text style={styles.titleLista}>{this.props.data.numero}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerLista: {
+    backgroundColor: "orange",
+    height: 80,
+    margin: 10,
+    color: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  titleLista: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  lugar: {
+    height: 20,
+    width: 20,
+    backgroundColor: "black",
+  },
+});
